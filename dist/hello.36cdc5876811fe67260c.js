593e1a4aef509e218ae6e3fef4b92f3a406b3d82
Ballsky22 <ballskyteam22@gmail.com>
2023-03-24 00:49:36 +0700

    Minimizer Webpack

---
+++ dist/hello.36cdc5876811fe67260c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbelajar_webpack_code=self.webpackChunkbelajar_webpack_code||[]).push([[858],{540:(a,i,m)=>{var l=m(486);const o=m.n(l)().find([{nama:"Ballsky",email:"Ballsky@gmail.com",motor:"Suzuki"},{nama:"Ari",email:"ari@gmail.com",motor:"Ducati"},{nama:"Miki",email:"miki@gmail.com",motor:"Honda"}],{nama:"Miki"});console.log(o)}},a=>{a(a.s=540)}]);
