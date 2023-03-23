b0b06b90467b59c41c3a96add4ad075085def716
Ballsky22 <ballskyteam22@gmail.com>
2023-03-23 23:58:46 +0700

    Assets Management Webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,23 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const config = require("./webpack.config");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(config, {
   mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+    compress: true,
+    port: 3000,
+    liveReload: true,
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
     clean: true,
+    assetModuleFilename: "img/[name][ext]",
   },
-  watch: true,
+  plugins: [new MiniCssExtractPlugin()],
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,19 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const config = require("./webpack.config");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(config, {
-  mode: "production",
+  mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.[contenthash].js",
+    assetModuleFilename: "img/[hash][ext]",
     clean: true,
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "main.[contenthash].css",
+    }),
+  ],
 });
